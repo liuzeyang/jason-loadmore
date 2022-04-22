22414933420696637f272ec519f277141ce59a3a
liuzeyang <331341107@qq.com>
2022-04-22 14:07:37 +0800

    feat: add .d

--- types/components.d.ts
+++
@@ -1,6 +0,0 @@
-import { ClassAttributes, DetailedReactHTMLElement, Key, ReactElement } from "react";
-export declare const injectLoadMore: (children: DetailedReactHTMLElement<any, HTMLElement>, props: ClassAttributes<HTMLElement> | {
-    className: string;
-    key: Key;
-    ref: unknown;
-}) => ReactElement;
--- types/default.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type ObserverOptions = {
-    root?: null | Element | Document | string;
-    rootMargin?: string;
-    threshold?: number | number[];
-};
-export declare const defaultOptions: ObserverOptions;
